bb4beadb54bd3a1bb9122a63264763c8dbb80964
foykon <foykon9@gmail.com>
2022-08-01 15:04:08 +0300

    13. gün sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,8 +1,10 @@
 ﻿using Business.Abstract;
+using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
@@ -11,6 +13,7 @@ using Entities.DTOs;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business.Concrete
@@ -18,20 +21,28 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
+        ICategoryService _categoryService;
 
-        public ProductManager(IProductDal productDal)
+       
+
+        public ProductManager(IProductDal productDal,ICategoryService categoryService)
         {
             _productDal = productDal;
+            _categoryService = categoryService;
         }
 
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-           
-
+            IResult result = BusinessRules.Run(CheckIfNameExists(product.ProductName),
+                CheckIfProductCountOfCategoryCorrect(product.CategoryId),
+                CheckIfCategoryLimitExceded());
+            if (result != null)
+            {
+                return result;
+            }
             _productDal.Add(product);
-            return new SuccessResult(Messages.ProductAdded);          
-            //return new Result(true, "Ürün eklendi");
+            return new SuccessResult(Messages.ProductAdded);
         }
 
         public IDataResult<List<Product>> GetAll()
@@ -60,5 +71,42 @@ namespace Business.Concrete
             if (DateTime.Now.Hour == 17) { return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime); }
             return new SuccessDataResult<List<ProductDetailDto>>( _productDal.GetProductDetails());
         }
+
+        [ValidationAspect(typeof(ProductValidator))]
+        public IResult Update(Product product)
+        {
+            
+            _productDal.Update(product);
+            return new SuccessResult(Messages.ProductAdded);
+        }
+
+        private IResult CheckIfProductCountOfCategoryCorrect(int categoryId)
+        {
+            //select count(*) from products where categoryId=1
+            var result = _productDal.GetAll(p => p.CategoryId == categoryId).Count;
+            if (result>15)
+            {
+                return new ErrorResult(Messages.ProductCountOfCategoryError);
+            }
+            return new SuccessResult();
+        }
+        private IResult CheckIfNameExists(string productName)
+        {
+            if (_productDal.GetAll(p => p.ProductName == productName).Any())
+            {
+                return new ErrorResult(Messages.ProductNameAlreadyExist);
+            }
+            return new SuccessResult();
+        }
+        private IResult CheckIfCategoryLimitExceded()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Data.Count > 15)
+            {
+                return new ErrorResult(Messages.CategoryLimitExceded);
+            }
+            return new SuccessResult();
+        }
+
     }
 }
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,8 @@ namespace Business.Constants
         public static string ProductDeleted = "Ürün silindi";
         public static string ProductsListed = "Ürünler listelendi";
         public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olablir";
+        public static string ProductNameAlreadyExist = "Bu isimde zaten başka bir ürün var";
+        internal static string CategoryLimitExceded = "15 den fazla categori mevcut";
     }
 }
